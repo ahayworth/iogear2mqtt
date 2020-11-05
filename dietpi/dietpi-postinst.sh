bb9ce3727004fb5aa6b07ade764c4b84e998ab0d
Andrew Hayworth <ahayworth@gmail.com>
2020-11-04 23:43:01 -0600

    fix postint script

--- dietpi/dietpi-postinst.sh
+++ dietpi/dietpi-postinst.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get -y install nodejs git
+apt-get -y install nodejs npm git build-essential
 
-git clone --shallow https://github.com/ahayworth/iogear2mqtt /opt/iogear2mqtt
+git clone --depth 1 https://github.com/ahayworth/iogear2mqtt /opt/iogear2mqtt
 
 cd /opt/iogear2mqtt
 npm install
