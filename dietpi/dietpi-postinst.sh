aaebcbd06c256c2bd94350f2060b41d5807ca90e
Andrew Hayworth <ahayworth@gmail.com>
2020-11-05 00:01:36 -0600

    more postinst fixes

--- dietpi/dietpi-postinst.sh
+++ dietpi/dietpi-postinst.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-apt-get -y install nodejs npm git build-essential
+apt-get -y install git build-essential
 
-git clone --depth 1 https://github.com/ahayworth/iogear2mqtt /opt/iogear2mqtt
+cd /opt
+
+wget https://unofficial-builds.nodejs.org/download/release/v14.9.0/node-v14.9.0-linux-armv6l.tar.gz
+tar xzf node-v14.9.0-linux-armv6l.tar.gz
+mv /opt/node-v14.9.0-linux-armv6l.tar.gz /opt/node
+ln -sf /opt/node/bin/node /usr/bin/node
+ln -sf /opt/node/bin/npm /usr/bin/npm
+ln -sf /opt/node/bin/npx /usr/bin/npx
+
+git clone --depth 1 https://github.com/ahayworth/iogear2mqtt
 
 cd /opt/iogear2mqtt
 npm install
